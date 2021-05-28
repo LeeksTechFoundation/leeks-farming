9f2ada7d2fd60b74aa98c4a14905a19b75154a9d
Kevin Koo <guxs@jmadao.com>
2021-05-29 02:36:12 +0800

    init

---
+++ precache-manifest.808fe2a58bf3f424ebca5ca8a47f2c0c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b7d80a4c77c1e8bd61e82b965aa5d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f36c43fd655cbaa087cb",
+    "url": "/static/css/main.ca414003.chunk.css"
+  },
+  {
+    "revision": "6afe51871c476e601e71",
+    "url": "/static/js/0.a2fac595.chunk.js"
+  },
+  {
+    "revision": "547e697f3a185b4a089d",
+    "url": "/static/js/10.bac4ed86.chunk.js"
+  },
+  {
+    "revision": "5fd74811a65733fd0414",
+    "url": "/static/js/11.60eaefc8.chunk.js"
+  },
+  {
+    "revision": "81e97ebe1232fc778fc7",
+    "url": "/static/js/3.fea5115b.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/static/js/3.fea5115b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518db65b39dbd57c9a42",
+    "url": "/static/js/4.f551a7c3.chunk.js"
+  },
+  {
+    "revision": "1b92026130dcf9d86fe5",
+    "url": "/static/js/5.45b07c2c.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/5.45b07c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e7837f88284fbf0c92",
+    "url": "/static/js/6.af968382.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.af968382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13e07eb676b9eb155c2",
+    "url": "/static/js/7.c8e9278e.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.c8e9278e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3befd0b06885b067a67",
+    "url": "/static/js/8.93bdb3fe.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.93bdb3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2672ab88dcd903b8223",
+    "url": "/static/js/9.6b6e2467.chunk.js"
+  },
+  {
+    "revision": "f36c43fd655cbaa087cb",
+    "url": "/static/js/main.29696351.chunk.js"
+  },
+  {
+    "revision": "d04f95d86cc4c4340633",
+    "url": "/static/js/runtime-main.75c03497.js"
+  },
+  {
+    "revision": "5cdac7fbeed368a0d7293ec381c2104f",
+    "url": "/static/media/github.5cdac7fb.png"
+  },
+  {
+    "revision": "fa7be2adf833402128bafed7a3a0b7f2",
+    "url": "/static/media/logo-uniswap.fa7be2ad.svg"
+  },
+  {
+    "revision": "92b8c33c368334ccc3a8f0c87eece154",
+    "url": "/static/media/logo.92b8c33c.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  }
+]);
